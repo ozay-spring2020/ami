509ba73519d133ba1d1d227beedddcf92ce2c540
yashoza-neu <oza.y@northeastern.edu>
2020-03-02 16:38:47 -0500

    adding CodeDeploy agent into ami1

--- requirements.sh
+++ requirements.sh
@@ -4,8 +4,8 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 
 # Install CodeDeploy Agent
-sudo apt-get install ruby
-sudo apt-get install wget
+sudo apt-get install ruby -y
+sudo apt-get install wget -y
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
