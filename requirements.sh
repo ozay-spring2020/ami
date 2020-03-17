179c5a056d6accb65cb0802f7ba38b4139ff023e
Yashoza19 <yoyashoza@gmail.com>
2020-03-17 14:48:07 -0400

    Adding CloudWatch Agent to AMI

--- requirements.sh
+++ requirements.sh
@@ -10,3 +10,7 @@ cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
+
+#Install CloudWatch Agent
+cd /home/ubuntu
+wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
